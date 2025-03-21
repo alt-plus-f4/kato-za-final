13b0a257c29205bf2bbbc4f1c8b1d044176d983c
alt-plus-f4 <valentin.assenov@abv.bg>
2025-03-21 11:46:20 +0200

    chore: remove console logs and error checks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,29 +16,20 @@ export const authOptions: NextAuthOptions = {
 				password: {},
 			},
 			async authorize(credentials) {
-				console.log(credentials);
-
-				if (!credentials?.email || !credentials.password) {
-					console.log('asd');
-					return null;
-				}
-				console.log('asd2');
+				if (!credentials?.email || !credentials.password) return null;
 
 				let user;
 				try {
 					user = await db.user.findUnique({
 						where: { email: credentials.email },
 					});
-					console.log('asd3');
 				} catch (error) {
-					console.error('Error while finding user:', error);
+					console.log('Error while finding user:', error);
 					return null;
 				}
 
 				if (user) {
-					if (user.password === credentials.password) {
-						return user;
-					}
+					if (user.password === credentials.password) return user;
 					return null;
 				}
 
